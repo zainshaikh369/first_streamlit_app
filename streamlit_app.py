8464def1d96d94781b6e2e8479f853d7bc3fbeeb
Zain Shaikh <94286211+zainshaikh369@users.noreply.github.com>
2022-09-08 11:44:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free_Range Egg')
